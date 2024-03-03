9917c9514a94e31a96a018eedab55ba07abc4d34
Mudassir Latif <mudassirm.latif@gmail.com>
2024-03-03 20:37:35 +0500

    third programme

---
+++ password_generator.py
@@ -0,0 +1,16 @@
+import random
+
+def password_generator(len):
+    ran=['a','b','c','d','e','f','g','h','i','j','k',
+            'l','m','n','o','p','q','r','s','t','u','v','w',
+            'x','y','z','0','1','2','3','4','5','6','7','8','9']
+    password=""
+    while len > 0:
+        pas=random.choice(ran)
+        len-=1
+        password+=pas
+    return password
+
+
+len=int(input("Enter the length of the password: "))
+print(password_generator(len))
