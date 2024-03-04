3f59adf1aaa7b5d4fd31d8e8c1b24a6eb3028d33
Mudassir Latif <mudassirm.latif@gmail.com>
2024-03-04 08:04:48 +0500

    fourth programme

---
+++ rock_papper_sissors.py
@@ -0,0 +1,46 @@
+import random 
+
+def game():
+    user_score=0
+    computer_score=0
+    while True:
+        choice=int(input("Enter your choice:\n1)play again\n2)view score\n3)terminate\n: "))
+
+        if choice==1:   
+            uinput=input("Enter your choice rock,papper or scissors: ")
+            lst=["rock","papper","scissors"]
+            cinput=random.choice(lst)
+
+            if cinput=="rock" and uinput=='rock' or cinput=="papper" and uinput=='papper' or cinput=="scissors" and uinput=='scissors':
+                print()
+                print("It's a draw!!!\n","User's input: ",uinput,"\nComputer's input: ",cinput, sep="")
+                print()
+                user_score+=1
+                computer_score+=1
+            
+            elif cinput=="papper" and uinput=='scissors' or cinput=="rock" and uinput=='papper' or cinput=="scissors" and uinput=='rock':
+                print()
+                print("Users's win!!!\n","User's input: ",uinput,"\nComputer's input: ",cinput, sep="")
+                print()
+                user_score+=2
+
+            elif uinput=="papper" and cinput=='scissors' or uinput=="papper" and cinput=='scissors' or uinput=="scissors" and cinput=='rock':
+                print()
+                print("Computer's win!!!\n","User's input: ",uinput,"\nComputer's input: ",cinput, sep="")
+                print()
+                computer_score+=2
+
+        elif choice==2:
+            print()
+            print("User's score:",user_score)
+            print("Computer's score:",computer_score)
+            print()
+
+        elif choice==3:
+            print("Programme terminates!!!")
+            break
+
+        else:
+            print("Invalid input!!!")
+     
+game()
